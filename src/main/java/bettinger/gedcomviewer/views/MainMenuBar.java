d2fbf18193182971ed3a3d78bf9e01759d0b9a58
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-06-04 12:44:24 +0200

    Fixed imports

--- src/main/java/bettinger/gedcomviewer/views/MainMenuBar.java
+++ src/main/java/bettinger/gedcomviewer/views/MainMenuBar.java
@@ -4,7 +4,6 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
