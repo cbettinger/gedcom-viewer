1445c19225f302b1b4573b7176786b4af0753bc1
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-06 10:20:36 +0200

    added class representing resulting face characteristic similarity

---
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FaceCharacteristicSimilarity.java
@@ -0,0 +1,33 @@
+package bettinger.gedcomviewer.tools.portraitcomparison.model;
+
+import java.awt.Image;
+
+public class FaceCharacteristicSimilarity {
+    private final float maxSimilarity;
+    private final float avgSimilarity;
+    private final Image targetPersonImage;
+    private final Image otherPersonImage;
+
+    public FaceCharacteristicSimilarity(float maxSimilarity, float avgSimilarity, Image targetPersonImage, Image otherPersonImage) {
+        this.maxSimilarity = maxSimilarity;
+        this.avgSimilarity = avgSimilarity;
+        this.targetPersonImage = targetPersonImage;
+        this.otherPersonImage = otherPersonImage;
+    }
+    
+    public float getMaxSimilarity() {
+        return maxSimilarity;
+    }
+
+    public float getAvgSimilarity() {
+        return avgSimilarity;
+    }
+
+    public Image getTargetPersonImage() {
+        return targetPersonImage;
+    }
+
+    public Image getOtherPersonImage() {
+        return otherPersonImage;
+    }
+}
