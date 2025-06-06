da9a4e18d2090dfaad5146bb9ee226dc9556e934
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-06 10:24:11 +0200

    renamed face charactristic to facial feature

--- src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FacialFeatureSimilarity.java
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FacialFeatureSimilarity.java
@@ -2,13 +2,13 @@ package bettinger.gedcomviewer.tools.portraitcomparison.model;
 
 import java.awt.Image;
 
-public class FaceCharacteristicSimilarity {
+public class FacialFeatureSimilarity {
     private final float maxSimilarity;
     private final float avgSimilarity;
     private final Image targetPersonImage;
     private final Image otherPersonImage;
 
-    public FaceCharacteristicSimilarity(float maxSimilarity, float avgSimilarity, Image targetPersonImage, Image otherPersonImage) {
+    public FacialFeatureSimilarity(float maxSimilarity, float avgSimilarity, Image targetPersonImage, Image otherPersonImage) {
         this.maxSimilarity = maxSimilarity;
         this.avgSimilarity = avgSimilarity;
         this.targetPersonImage = targetPersonImage;
