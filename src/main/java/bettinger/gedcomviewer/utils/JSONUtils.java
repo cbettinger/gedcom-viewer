5c3a36dde283eac961e983259f4ac7d8b7e125c0
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-06 10:09:57 +0200

    added method to parse json strings to json objects

--- src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
+++ src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public interface JSONUtils {
@@ -16,5 +17,15 @@ public interface JSONUtils {
 			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to serialize object to JSON string", e);
 			return "{}";
 		}
+	}
+
+	public static JsonNode fromString(final String jsonString){
+			final var mapper = new ObjectMapper();
+            try {
+				return mapper.readTree(jsonString);
+			} catch (JsonProcessingException e) {
+				Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to deserialize JSON string to JSON object", e);
+				return mapper.createObjectNode();
+			}
 	}
 }
