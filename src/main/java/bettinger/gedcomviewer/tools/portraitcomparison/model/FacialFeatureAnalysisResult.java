6a413bef250c3614a6269699d2770c66ab1ae1c5
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-06 15:50:44 +0200

    added fromJSON

--- src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FacialFeatureAnalysisResult.java
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FacialFeatureAnalysisResult.java
@@ -2,6 +2,8 @@ package bettinger.gedcomviewer.tools.portraitcomparison.model;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class FacialFeatureAnalysisResult {
     private final HashMap<String, FacialFeatureSimilarity> personSimilarities;
     private final HashMap<AncestralLine, Float> pathSimilarities;
@@ -17,5 +19,27 @@ public class FacialFeatureAnalysisResult {
 
     public HashMap<AncestralLine, Float> getPathSimilarities() {
         return pathSimilarities;
+    }
+
+    public static FacialFeatureAnalysisResult fromJSON(final JsonNode json, final String facialFeature) {
+        final JsonNode personSimilaritiesNode = json.get("nodes").get(facialFeature);
+        final JsonNode pathSimilaritiesNode = json.get("pathSimilarities").get(facialFeature);
+
+        HashMap<String, FacialFeatureSimilarity> personSimilarities = new HashMap<>();
+        HashMap<AncestralLine, Float> pathSimilarities = new HashMap<>();
+
+        final var personSimilarityEntries = personSimilaritiesNode.fields();
+        while (personSimilarityEntries.hasNext()) {
+            final var entry = personSimilarityEntries.next();
+            personSimilarities.put(entry.getKey(), FacialFeatureSimilarity.fromJSON(entry.getValue()));
+        }
+
+        final var pathSimilarityEntries = pathSimilaritiesNode.fields();
+        while (pathSimilarityEntries.hasNext()) {
+            final var entry = pathSimilarityEntries.next();
+            pathSimilarities.put(AncestralLine.fromString(entry.getKey()), Float.parseFloat(entry.getValue().asText()));
+        }
+
+        return new FacialFeatureAnalysisResult(personSimilarities, pathSimilarities);
     }
 }
