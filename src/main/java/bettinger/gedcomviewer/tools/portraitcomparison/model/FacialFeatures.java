72977f87a25b5b3be088dfe178de8b4a3e4a337f
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-06 16:05:56 +0200

    added enum for facial features

---
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/model/FacialFeatures.java
@@ -0,0 +1,11 @@
+package bettinger.gedcomviewer.tools.portraitcomparison.model;
+
+public enum FacialFeatures {
+    CHEEKS,
+    CHIN,
+    EYEBROWS,
+    EYES,
+    FACESHAPE,
+    LIPS,
+    NOSE;
+}
