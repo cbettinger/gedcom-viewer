f2364c2fe5ccae9e0c7de18fa9d48e438aede595
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-10 09:19:18 +0200

    made vbox content non stretching if wanted

--- src/main/java/bettinger/gedcomviewer/views/VBox.java
+++ src/main/java/bettinger/gedcomviewer/views/VBox.java
@@ -1,12 +1,38 @@
 package bettinger.gedcomviewer.views;
 
+import java.awt.Component;
+
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class VBox extends JPanel{
+
+    private JPanel outerPanel;
+
     public VBox() {
         super();
-
+        this.outerPanel = null;
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+    }
+
+    public VBox(boolean stretchableComponents)  {
+        super();
+        if (stretchableComponents) {
+            this.outerPanel = null;
+            setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        } else {
+            this.outerPanel = new JPanel();
+            this.outerPanel.setLayout(new BoxLayout(this.outerPanel, BoxLayout.PAGE_AXIS));
+
+            super.add(this.outerPanel);
+        }
+    }
+
+    @Override
+    public Component add(Component comp) {
+        if (outerPanel == null) {
+            return add(comp);
+        }
+        return outerPanel.add(comp);
     }
 }
