d6885db412a4c1680ac84d1536cb3a426e398c9f
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-06-10 11:06:24 +0200

    Moved creation of IDs by hashCode to Structure

--- src/main/java/bettinger/gedcomviewer/model/Occupation.java
+++ src/main/java/bettinger/gedcomviewer/model/Occupation.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
 
-import bettinger.gedcomviewer.Format;
 import bettinger.gedcomviewer.I18N;
 import bettinger.gedcomviewer.utils.HTMLUtils;
 
@@ -21,7 +20,7 @@ public class Occupation extends Structure implements DerivedRecord {
 	private final List<Individual> individuals;
 
 	Occupation(final GEDCOM gedcom, final String name, final List<Individual> individuals) {
-		super(gedcom, getId(name), null);
+		super(gedcom, constructId(TAG, name), null);
 
 		this.recordManager = new RecordManager(this, gedcom, individuals.stream().map(Record::getLastChange).filter(Objects::nonNull).min(Comparator.naturalOrder()).orElse(null));
 
@@ -97,10 +96,6 @@ public class Occupation extends Structure implements DerivedRecord {
 	/* #endregion */
 
 	static String getLinkFromFact(final Fact occupationFact) {
-		return HTMLUtils.createAnchorLink(getId(occupationFact.getValue()), occupationFact.getValue());
-	}
-
-	private static String getId(final String name) {
-		return String.format(Format.PIPE_SEPARATED, TAG, name.hashCode());
+		return HTMLUtils.createAnchorLink(constructId(TAG, occupationFact.getValue()), occupationFact.getValue());
 	}
 }
--- src/main/java/bettinger/gedcomviewer/model/Substructure.java
+++ src/main/java/bettinger/gedcomviewer/model/Substructure.java
@@ -2,14 +2,12 @@ package bettinger.gedcomviewer.model;
 
 import org.folg.gedcom.model.ExtensionContainer;
 
-import bettinger.gedcomviewer.Format;
-
 abstract class Substructure extends Structure {
 
 	final Structure parentStructure;
 
 	Substructure(final GEDCOM gedcom, final String idPrefix, final ExtensionContainer wrappedStructure, final Structure parentStructure) {
-		super(gedcom, String.format(Format.PIPE_SEPARATED, idPrefix, wrappedStructure.hashCode()), wrappedStructure);
+		super(gedcom, constructId(idPrefix, wrappedStructure), wrappedStructure);
 
 		this.parentStructure = parentStructure;
 	}
