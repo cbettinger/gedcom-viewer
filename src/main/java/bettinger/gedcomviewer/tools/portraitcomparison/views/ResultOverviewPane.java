97e5351155c2d4728e0a5e684a3a0e148c271c68
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-13 10:06:27 +0200

    added table to overview

--- src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/ResultOverviewPane.java
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/ResultOverviewPane.java
@@ -1,16 +1,63 @@
 package bettinger.gedcomviewer.tools.portraitcomparison.views;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 
+import bettinger.gedcomviewer.I18N;
 import bettinger.gedcomviewer.tools.portraitcomparison.model.FacialFeatureAnalysisResult;
 import bettinger.gedcomviewer.tools.portraitcomparison.model.FacialFeatures;
+import bettinger.gedcomviewer.views.AutoFitTable;
 
 public class ResultOverviewPane extends JPanel {
     
     public ResultOverviewPane(final TreeMap<FacialFeatures, FacialFeatureAnalysisResult> results) {
         super();
+        setLayout(new BorderLayout());
+
+        final Map<FacialFeatures, Color> FEATURE_COLORS = ResultOverviewPane.getFeatureColors();
+
+        var visualization = new JPanel();
+        TreeMap<FacialFeatures, Object[]> maxPathSimilarities = new TreeMap<>();
+        TreeMap<FacialFeatures, Object[]> maxPersonSimilarities = new TreeMap<>();
+        final String[] columns = {I18N.get("FacialFeature"), I18N.get("LineColor"), I18N.get("AvgSimilarity"), I18N.get("MaxSimilarity")};
+        ArrayList<Object[]> tableData = new ArrayList<>();
+
+        for (final var entry : results.entrySet()) {
+           final var feature = entry.getKey();
+           final var featureResult = entry.getValue();
+           final var maxPathSimilarity = featureResult.getMaxPathSimilarity();
+           maxPathSimilarities.put(feature, maxPathSimilarity);
+           final var maxPersonSimilarity = featureResult.getMaxPersonSimilarity();
+           maxPersonSimilarities.put(feature, maxPersonSimilarity);
+
+           Object[] row = {feature, feature, maxPathSimilarity[1], maxPersonSimilarity[1]};
+           tableData.add(row);
+        }
+
+        var legend = new AutoFitTable();
+        Object[][] data = tableData.toArray(new Object[0][0]);
+        legend.setModel(new DefaultTableModel(data, columns));
+
+        add(legend, BorderLayout.LINE_END);
+    }
+
+    public static Map<FacialFeatures, Color> getFeatureColors() {
+        final Map<FacialFeatures, Color> FEATURE_COLORS = new HashMap<>();
+        FEATURE_COLORS.put(FacialFeatures.CHEEKS, Color.BLUE);
+        FEATURE_COLORS.put(FacialFeatures.CHIN, Color.GREEN);
+        FEATURE_COLORS.put(FacialFeatures.EYEBROWS, Color.ORANGE);
+        FEATURE_COLORS.put(FacialFeatures.EYES, Color.RED);
+        FEATURE_COLORS.put(FacialFeatures.FACESHAPE, Color.CYAN);
+        FEATURE_COLORS.put(FacialFeatures.LIPS, Color.MAGENTA);
+        FEATURE_COLORS.put(FacialFeatures.NOSE, Color.YELLOW);
+
+        return FEATURE_COLORS;
     }
 }
