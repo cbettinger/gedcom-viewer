f2349cb8f4b4a78649865d670d88aa557575f9a0
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-06-13 11:25:47 +0200

    Added positron layer

--- src/main/resources/webview/map/main.js
+++ src/main/resources/webview/map/main.js
@@ -27,20 +27,27 @@ function addMap() {
 		accessToken: "pk.eyJ1IjoiY2JldHRpbmdlciIsImEiOiJja2Q0cnc3aGUxdTF1MnNvNzRnN2pvbmpxIn0.wDahEhYH1fzw7OmTrs9OZw"
 	}).addTo(map);
 
-	let voyagerLayer = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://carto.com/attributions">CARTO</a>',
-		maxZoom: 19,
-	});
-
 	let osmLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	});
 
+	let voyagerLayer = L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		maxZoom: 19,
+	});
+
+	let positronLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		subdomains: 'abcd',
+		maxZoom: 20,
+	});
+
 	L.control.layers({
 		MapBox: mapBoxLayer,
-		Voyager: voyagerLayer,
 		OSM: osmLayer,
+		Voyager: voyagerLayer,
+		Positron: positronLayer,
 	}).addTo(map);
 }
 
