3c30d219c4d0906bec1c02eddc9058b5a44efc22
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-17 11:02:07 +0200

    renamed renderer

--- src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/ResultOverviewRenderer.java
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/ResultOverviewRenderer.java
@@ -1,12 +1,9 @@
 package bettinger.gedcomviewer.tools.portraitcomparison.views;
 
-import javax.swing.JTabbedPane;
-
-import bettinger.gedcomviewer.Preferences;
 import bettinger.gedcomviewer.model.Individual;
-import bettinger.gedcomviewer.model.LineageMode;
 import bettinger.gedcomviewer.views.visualization.AncestorsRenderer;
 
-class ResultTreeRenderer extends AncestorsRenderer {
+class ResultOverviewRenderer extends AncestorsRenderer {
+
 }
 
