4927b1747914afdd41de8a9ec7677445155e43ce
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-06-30 19:16:34 +0200

    adjusted size of details legend

--- src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/DetailedResultPane.java
+++ src/main/java/bettinger/gedcomviewer/tools/portraitcomparison/views/DetailedResultPane.java
@@ -2,6 +2,7 @@ package bettinger.gedcomviewer.tools.portraitcomparison.views;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,6 +21,8 @@ public class DetailedResultPane extends JPanel {
 
     static final Color PERFECT_MATCH_COLOR = Color.GREEN;
     static final Color NO_MATCH_COLOR = Color.DARK_GRAY;
+    static final int LEGEND_WIDTH = 400;
+    static final int COLOR_RAMP_HEIGHT = 100;
 
     private final WebViewPanel visualization;
 
@@ -40,6 +43,7 @@ public class DetailedResultPane extends JPanel {
         colorRampDescription.add(new JLabel("100%"), BorderLayout.NORTH);
         colorRampDescription.add(new JLabel("0%"), BorderLayout.SOUTH);
         colorRampPane.add(colorRampDescription);
+        colorRampPane.setPreferredSize(new Dimension(LEGEND_WIDTH, COLOR_RAMP_HEIGHT));
 
         var explanations = new JTextArea();
         explanations.setEditable(false);
@@ -49,8 +53,9 @@ public class DetailedResultPane extends JPanel {
         legend.add(colorRampPane);
         legend.add(explanations);
 
-        add(new JScrollPane(legend), BorderLayout.EAST);
+        add(legend, BorderLayout.EAST);
         add(visualization, BorderLayout.CENTER);
+
         update(proband, numGenerations, result);
     }
 
