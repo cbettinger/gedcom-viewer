b720281415b055189b24f71aadeecc01c23c27f2
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-07-02 09:25:37 +0200

    formatted java code

--- src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
+++ src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
@@ -21,20 +21,20 @@ public interface JSONUtils {
 		}
 	}
 
-	public static JsonNode fromString(final String jsonString){
-			final var mapper = new ObjectMapper();
-            try {
-				return mapper.readTree(jsonString);
-			} catch (JsonProcessingException e) {
-				Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to deserialize JSON string to JSON object", e);
-				return mapper.createObjectNode();
-			}
+	public static JsonNode fromString(final String jsonString) {
+		final var mapper = new ObjectMapper();
+		try {
+			return mapper.readTree(jsonString);
+		} catch (JsonProcessingException e) {
+			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to deserialize JSON string to JSON object", e);
+			return mapper.createObjectNode();
+		}
 	}
 
 	public static File toJSONFile(final Object input, final String filePath) {
 		final var writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		File file = new File(filePath);
-        try {
+		try {
 			writer.writeValue(file, input);
 		} catch (IOException e) {
 			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to create JSON file", e);
