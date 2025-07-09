9e6b74a554c6fea746ee1c5563773f8315f84763
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-09 10:45:22 +0200

    Improved JSONUtils

--- src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
+++ src/main/java/bettinger/gedcomviewer/utils/JSONUtils.java
@@ -13,6 +13,7 @@ public interface JSONUtils {
 
 	public static String toJSON(final Object input) {
 		final var writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
 		try {
 			return writer.writeValueAsString(input);
 		} catch (final JsonProcessingException e) {
@@ -21,24 +22,26 @@ public interface JSONUtils {
 		}
 	}
 
-	public static JsonNode fromString(final String jsonString) {
-		final var mapper = new ObjectMapper();
-		try {
-			return mapper.readTree(jsonString);
-		} catch (JsonProcessingException e) {
-			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to deserialize JSON string to JSON object", e);
-			return mapper.createObjectNode();
-		}
-	}
-
 	public static File toJSONFile(final Object input, final String filePath) {
 		final var writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
-		File file = new File(filePath);
+		final var file = new File(filePath);
+
 		try {
 			writer.writeValue(file, input);
-		} catch (IOException e) {
-			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to create JSON file", e);
+		} catch (final IOException e) {
+			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to serialize object to JSON file", e);
 		}
 		return file;
+	}
+
+	public static JsonNode fromString(final String input) {
+		final var reader = new ObjectMapper();
+
+		try {
+			return reader.readTree(input);
+		} catch (final JsonProcessingException e) {
+			Logger.getLogger(JSONUtils.class.getName()).log(Level.SEVERE, "Failed to deserialize JSON string to JSON node", e);
+			return reader.createObjectNode();
+		}
 	}
 }
