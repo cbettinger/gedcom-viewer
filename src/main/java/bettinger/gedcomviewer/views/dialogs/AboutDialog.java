d63127534b464e727b62d95de240b97347b47217
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-11 17:04:15 +0200

    Renamed folder

--- src/main/java/bettinger/gedcomviewer/views/dialogs/AboutDialog.java
+++ src/main/java/bettinger/gedcomviewer/views/dialogs/AboutDialog.java
@@ -48,7 +48,7 @@ public class AboutDialog extends JDialog {
 		HTMLUtils.appendLine(sb, "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.");
 
 		HTMLUtils.appendH2(sb, "Third-party software");
-		HTMLUtils.appendLine(sb, "See subfolder \"thirdparty\" for a list of third-party software products and media assets that are redistributed and/or used in accordance to their respective license.");
+		HTMLUtils.appendLine(sb, "See subfolder \"vendor\" for a list of third-party software products and media assets that are redistributed and/or used in accordance to their respective license.");
 
 		textPane.setText(sb.toString());
 
