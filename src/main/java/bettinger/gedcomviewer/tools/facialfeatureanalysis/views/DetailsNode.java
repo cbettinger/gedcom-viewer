63b2e6d10869c2bea778a2ad9cb8b8ac4e309d28
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-11 10:29:33 +0200

    Formatting

--- src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/views/DetailsNode.java
+++ src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/views/DetailsNode.java
@@ -18,101 +18,101 @@ import bettinger.gedcomviewer.tools.facialfeatureanalysis.Similarity;
 import bettinger.gedcomviewer.views.visualization.Node;
 
 public class DetailsNode extends Node {
-    private static final int BORDER_THICKNESS = 3;
+	private static final int BORDER_THICKNESS = 3;
 
-    private Image portraitTargetPerson;
-    private int portraitTargetPersonWidth;
+	private Image portraitTargetPerson;
+	private int portraitTargetPersonWidth;
 
-    private Color borderColor;
+	private Color borderColor;
 
-    DetailsNode(SVGGraphics2D g, Individual individual, boolean isClone, Node parentNode) {
-        super(g, individual, isClone, parentNode);
+	DetailsNode(SVGGraphics2D g, Individual individual, boolean isClone, Node parentNode) {
+		super(g, individual, isClone, parentNode);
 
-        this.portraitTargetPerson = null;
-        this.portraitTargetPersonWidth = 0;
-        this.borderColor = null;
-    }
+		this.portraitTargetPerson = null;
+		this.portraitTargetPersonWidth = 0;
+		this.borderColor = null;
+	}
 
-    void init(Individual target, Similarity similarity) {
-        if (similarity != null) {
-            portrait = getPortrait(individual, similarity.getMaxSimilarityAncestorPortrait());
-            portraitWidth = portrait == null ? 0 : portrait.getWidth(null);
+	void init(Individual target, Similarity similarity) {
+		if (similarity != null) {
+			portrait = getPortrait(individual, similarity.getMaxSimilarityAncestorPortrait());
+			portraitWidth = portrait == null ? 0 : portrait.getWidth(null);
 
-            portraitTargetPerson = getPortrait(target, similarity.getMaxSimilarityProbandsPortrait());
-            portraitTargetPersonWidth = portraitTargetPerson == null ? 0 : portraitTargetPerson.getWidth(null);
+			portraitTargetPerson = getPortrait(target, similarity.getMaxSimilarityProbandsPortrait());
+			portraitTargetPersonWidth = portraitTargetPerson == null ? 0 : portraitTargetPerson.getWidth(null);
 
-            text = getTextLines(similarity.getAvgSimilarity(), similarity.getMaxSimilarity());
+			text = getTextLines(similarity.getAvgSimilarity(), similarity.getMaxSimilarity());
 
-            g.setFont(BOLD_FONT);
-            final var fontMetrics = g.getFontMetrics();
-            final var maximalLineWidth = fontMetrics.stringWidth(text.stream().max(Comparator.comparing(fontMetrics::stringWidth)).orElse(""));
-            this.width = Math.max(MINIMAL_WIDTH, maximalLineWidth + 3 * PADDING + (this.portrait == null ? 0 : this.portraitWidth + PADDING) + (this.portraitTargetPerson == null ? 0 : this.portraitTargetPersonWidth + PADDING));
-            this.height = Math.max(MINIMAL_HEIGHT, text.size() * (lineHeight + PADDING) + 2 * PADDING);
+			g.setFont(BOLD_FONT);
+			final var fontMetrics = g.getFontMetrics();
+			final var maximalLineWidth = fontMetrics.stringWidth(text.stream().max(Comparator.comparing(fontMetrics::stringWidth)).orElse(""));
+			this.width = Math.max(MINIMAL_WIDTH, maximalLineWidth + 3 * PADDING + (this.portrait == null ? 0 : this.portraitWidth + PADDING) + (this.portraitTargetPerson == null ? 0 : this.portraitTargetPersonWidth + PADDING));
+			this.height = Math.max(MINIMAL_HEIGHT, text.size() * (lineHeight + PADDING) + 2 * PADDING);
 
-            int red = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getRed() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getRed()));
-            int green = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getGreen() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getGreen()));
-            int blue = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getBlue() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getBlue()));
-            this.borderColor = new Color(red, green, blue, 255);
-        }
-    }
+			int red = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getRed() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getRed()));
+			int green = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getGreen() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getGreen()));
+			int blue = Math.min(255, (int) (DetailsPane.NO_MATCH_COLOR.getBlue() + similarity.getAvgSimilarity() * DetailsPane.PERFECT_MATCH_COLOR.getBlue()));
+			this.borderColor = new Color(red, green, blue, 255);
+		}
+	}
 
-    private Image getPortrait(Individual individual, String filePath) {
-        Image result = null;
+	private Image getPortrait(Individual individual, String filePath) {
+		Image result = null;
 
-        if (individual != null) {
-            final var portraits = individual.getFacialPortraits();
-            for (var media : portraits) {
-                if (media.getFilePath().equals(filePath) && media.exists()) {
-                    return individual.getClippedImage(media, -1, PORTRAIT_HEIGHT);
-                }
-            }
-        }
+		if (individual != null) {
+			final var portraits = individual.getFacialPortraits();
+			for (var media : portraits) {
+				if (media.getFilePath().equals(filePath) && media.exists()) {
+					return individual.getClippedImage(media, -1, PORTRAIT_HEIGHT);
+				}
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    private List<String> getTextLines(Float avgSimilarity, Float maxSimilarity) {
-        final List<String> result = new ArrayList<>();
+	private List<String> getTextLines(Float avgSimilarity, Float maxSimilarity) {
+		final List<String> result = new ArrayList<>();
 
-        if (individual == null) {
-            result.add(Structure.UNKNOWN_STRING);
-        } else {
-            final var name = individual.getNickname().isEmpty() ? individual.getName() : String.format(Format.STRING_WITH_QUOTED_SUFFIX, individual.getName(), individual.getNickname());
-            result.add(name);
+		if (individual == null) {
+			result.add(Structure.UNKNOWN_STRING);
+		} else {
+			final var name = individual.getNickname().isEmpty() ? individual.getName() : String.format(Format.STRING_WITH_QUOTED_SUFFIX, individual.getName(), individual.getNickname());
+			result.add(name);
 
-            final var avgSim = String.format("%s: %.2f%%", I18N.get("AvgSimilarity"), avgSimilarity * 100);
-            result.add(avgSim);
+			final var avgSim = String.format("%s: %.2f%%", I18N.get("AvgSimilarity"), avgSimilarity * 100);
+			result.add(avgSim);
 
-            final var maxSim = String.format("%s: %.2f%%", I18N.get("MaxSimilarity"), maxSimilarity * 100);
-            result.add(maxSim);
-        }
+			final var maxSim = String.format("%s: %.2f%%", I18N.get("MaxSimilarity"), maxSimilarity * 100);
+			result.add(maxSim);
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    @Override
-    protected void renderPortraits() {
-        super.renderPortraits();
-        if (portraitTargetPerson != null) {
-            g.drawImage(portraitTargetPerson, x + portraitWidth + 2 * PADDING, y + PADDING, portraitTargetPersonWidth, PORTRAIT_HEIGHT, null);
-        }
-    }
+	@Override
+	protected void renderPortraits() {
+		super.renderPortraits();
+		if (portraitTargetPerson != null) {
+			g.drawImage(portraitTargetPerson, x + portraitWidth + 2 * PADDING, y + PADDING, portraitTargetPersonWidth, PORTRAIT_HEIGHT, null);
+		}
+	}
 
-    @Override
-    public void render(int x, int y) {
-        super.render(x, y);
-        if (borderColor != null) {
-            final Stroke defaultStroke = g.getStroke();
-            g.setStroke(new BasicStroke(BORDER_THICKNESS));
-            g.setPaint(borderColor);
-            g.drawRect(this.x, this.y, this.width, this.height);
-            g.setPaint(Color.BLACK);
-            g.setStroke(defaultStroke);
-        }
-    }
+	@Override
+	public void render(int x, int y) {
+		super.render(x, y);
+		if (borderColor != null) {
+			final Stroke defaultStroke = g.getStroke();
+			g.setStroke(new BasicStroke(BORDER_THICKNESS));
+			g.setPaint(borderColor);
+			g.drawRect(this.x, this.y, this.width, this.height);
+			g.setPaint(Color.BLACK);
+			g.setStroke(defaultStroke);
+		}
+	}
 
-    @Override
-    protected int getTextPositionX() {
-        return super.getTextPositionX() + (portraitTargetPerson == null ? 0 : this.portraitTargetPersonWidth + 2 * PADDING);
-    }
+	@Override
+	protected int getTextPositionX() {
+		return super.getTextPositionX() + (portraitTargetPerson == null ? 0 : this.portraitTargetPersonWidth + 2 * PADDING);
+	}
 }
--- src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/views/DetailsPane.java
+++ src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/views/DetailsPane.java
@@ -17,60 +17,60 @@ import bettinger.gedcomviewer.tools.facialfeatureanalysis.AnalysisResult;
 import bettinger.gedcomviewer.views.WebViewPanel;
 
 public class DetailsPane extends JPanel {
-  static final Color PERFECT_MATCH_COLOR = Color.GREEN;
-  static final Color NO_MATCH_COLOR = Color.DARK_GRAY;
-  static final int LEGEND_WIDTH = 400;
-  static final int COLOR_RAMP_HEIGHT = 100;
+	static final Color PERFECT_MATCH_COLOR = Color.GREEN;
+	static final Color NO_MATCH_COLOR = Color.DARK_GRAY;
+	static final int LEGEND_WIDTH = 400;
+	static final int COLOR_RAMP_HEIGHT = 100;
 
-  private final WebViewPanel visualization;
+	private final WebViewPanel visualization;
 
-  public DetailsPane(final Individual proband, final int generations, final AnalysisResult result) {
-    setLayout(new BorderLayout());
+	public DetailsPane(final Individual proband, final int generations, final AnalysisResult result) {
+		setLayout(new BorderLayout());
 
-    this.visualization = new WebViewPanel();
+		this.visualization = new WebViewPanel();
 
-    var legend = new JPanel();
-    legend.setLayout(new BoxLayout(legend, BoxLayout.Y_AXIS));
+		var legend = new JPanel();
+		legend.setLayout(new BoxLayout(legend, BoxLayout.Y_AXIS));
 
-    var colorGradientPane = new JPanel();
-    colorGradientPane.setLayout(new BoxLayout(colorGradientPane, BoxLayout.X_AXIS));
-    colorGradientPane.add(new GradientPanel(PERFECT_MATCH_COLOR, NO_MATCH_COLOR));
+		var colorGradientPane = new JPanel();
+		colorGradientPane.setLayout(new BoxLayout(colorGradientPane, BoxLayout.X_AXIS));
+		colorGradientPane.add(new GradientPanel(PERFECT_MATCH_COLOR, NO_MATCH_COLOR));
 
-	var colorGradientDescription = new JPanel();
-    colorGradientDescription.setLayout(new BorderLayout());
-    colorGradientDescription.add(new JLabel("100%"), BorderLayout.NORTH);
-    colorGradientDescription.add(new JLabel("0%"), BorderLayout.SOUTH);
-    colorGradientPane.add(colorGradientDescription);
-    colorGradientPane.setPreferredSize(new Dimension(LEGEND_WIDTH, COLOR_RAMP_HEIGHT));
+		var colorGradientDescription = new JPanel();
+		colorGradientDescription.setLayout(new BorderLayout());
+		colorGradientDescription.add(new JLabel("100%"), BorderLayout.NORTH);
+		colorGradientDescription.add(new JLabel("0%"), BorderLayout.SOUTH);
+		colorGradientPane.add(colorGradientDescription);
+		colorGradientPane.setPreferredSize(new Dimension(LEGEND_WIDTH, COLOR_RAMP_HEIGHT));
 
-    var explanations = new JTextArea();
-    explanations.setEditable(false);
-    explanations.setLineWrap(true);
-    explanations.setText(String.format("\n%s\n\n%s: %s\n\n%s: %s", I18N.get("PathSimilarityDetailsExplanation"), I18N.get("AvgSimilarity"), I18N.get("AvgSimilarityDetailsExplanation"), I18N.get("MaxSimilarity"), I18N.get("MaxSimilarityDetailsExplanation")));
+		var explanations = new JTextArea();
+		explanations.setEditable(false);
+		explanations.setLineWrap(true);
+		explanations.setText(String.format("\n%s\n\n%s: %s\n\n%s: %s", I18N.get("PathSimilarityDetailsExplanation"), I18N.get("AvgSimilarity"), I18N.get("AvgSimilarityDetailsExplanation"), I18N.get("MaxSimilarity"), I18N.get("MaxSimilarityDetailsExplanation")));
 
-    legend.add(colorGradientPane);
-    legend.add(explanations);
+		legend.add(colorGradientPane);
+		legend.add(explanations);
 
-    add(legend, BorderLayout.EAST);
-    add(visualization, BorderLayout.CENTER);
+		add(legend, BorderLayout.EAST);
+		add(visualization, BorderLayout.CENTER);
 
-    update(proband, generations, result);
-  }
+		update(proband, generations, result);
+	}
 
-  private void update(final Individual proband, final int numGenerations, AnalysisResult result) {
-    DetailsRenderer renderer = null;
+	private void update(final Individual proband, final int numGenerations, AnalysisResult result) {
+		DetailsRenderer renderer = null;
 
-    try {
-      renderer = new DetailsRenderer(proband, result);
-    } catch (final Exception e) {
-      Logger.getLogger(DetailsPane.class.getName()).log(Level.SEVERE, "Failed to create renderer", e);
-    }
+		try {
+			renderer = new DetailsRenderer(proband, result);
+		} catch (final Exception e) {
+			Logger.getLogger(DetailsPane.class.getName()).log(Level.SEVERE, "Failed to create renderer", e);
+		}
 
-    if (renderer != null) {
-      renderer.render(proband, numGenerations + 1);
+		if (renderer != null) {
+			renderer.render(proband, numGenerations + 1);
 
-      visualization.setBody(renderer.toString());
-      visualization.scrollTo(renderer.getProbandNode().getPosition());
-    }
-  }
+			visualization.setBody(renderer.toString());
+			visualization.scrollTo(renderer.getProbandNode().getPosition());
+		}
+	}
 }
