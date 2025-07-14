d17017656c0f70554785e2ea234842b1c4adaf37
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-14 12:19:26 +0200

    Moved folder

--- src/main/resources/tools/facialfeatureanalysis/classifiers/XGBClassifiers.py
+++ src/main/resources/tools/facialfeatureanalysis/classifiers/XGBClassifiers.py
@@ -4,33 +4,33 @@ XGB_CLASSIFIERS = {
     "CHEEKS": {
         "first": XGBooster(
             "xgb_20_6_cheeks_first",
-            "xgboost/xgb_20_6_cheeks_edgeDifference_first.json",
+            "classifiers/xgboost/xgb_20_6_cheeks_edgeDifference_first.json",
             20,
             6,
         ),
         "second": XGBooster(
             "xgb_20_6_cheeks_second",
-            "xgboost/xgb_20_6_cheeks_edgeDifference_second.json",
+            "classifiers/xgboost/xgb_20_6_cheeks_edgeDifference_second.json",
             20,
             6,
         ),
     },
     "CHIN": XGBooster(
         "xgb_20_6_chin",
-        "xgboost/xgb_20_6_chin_edgeDifference.json",
+        "classifiers/xgboost/xgb_20_6_chin_edgeDifference.json",
         20,
         6,
     ),
     "EYEBROWS": {
         "first": XGBooster(
             "xgb_20_6_eyebrows_first",
-            "xgboost/xgb_20_6_eyebrows_edgeDifference_first.json",
+            "classifiers/xgboost/xgb_20_6_eyebrows_edgeDifference_first.json",
             20,
             6,
         ),
         "second": XGBooster(
             "xgb_20_6_eyebrows_second",
-            "xgboost/xgb_20_6_eyebrows_edgeDifference_second.json",
+            "classifiers/xgboost/xgb_20_6_eyebrows_edgeDifference_second.json",
             20,
             6,
         ),
@@ -38,40 +38,40 @@ XGB_CLASSIFIERS = {
     "EYES": {
         "first": XGBooster(
             "xgb_20_6_eyes_first",
-            "xgboost/xgb_20_6_eyes_edgeDifference_first.json",
+            "classifiers/xgboost/xgb_20_6_eyes_edgeDifference_first.json",
             20,
             6,
         ),
         "second": XGBooster(
             "xgb_20_6_eyes_second",
-            "xgboost/xgb_20_6_eyes_edgeDifference_second.json",
+            "classifiers/xgboost/xgb_20_6_eyes_edgeDifference_second.json",
             20,
             6,
         ),
     },
     "FACESHAPE": XGBooster(
         "xgb_20_6_faceshape",
-        "xgboost/xgb_20_6_faceshape_edgeDifference.json",
+        "classifiers/xgboost/xgb_20_6_faceshape_edgeDifference.json",
         20,
         6,
     ),
     "LIPS": {
         "first": XGBooster(
             "xgb_20_6_lips_first",
-            "xgboost/xgb_20_6_lips_edgeDifference_first.json",
+            "classifiers/xgboost/xgb_20_6_lips_edgeDifference_first.json",
             20,
             6,
         ),
         "second": XGBooster(
             "xgb_20_6_lips_second",
-            "xgboost/xgb_20_6_lips_edgeDifference_second.json",
+            "classifiers/xgboost/xgb_20_6_lips_edgeDifference_second.json",
             20,
             6,
         ),
     },
     "NOSE": XGBooster(
         "xgb_20_6_nose",
-        "xgboost/xgb_20_6_nose_edgeDifference.json",
+        "classifiers/xgboost/xgb_20_6_nose_edgeDifference.json",
         20,
         6,
     ),
