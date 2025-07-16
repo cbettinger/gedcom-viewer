bb7993b78c1d88c69291f93e7a46d49ce562f470
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-16 16:52:56 +0200

    Improved color palette

--- src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/FacialFeature.java
+++ src/main/java/bettinger/gedcomviewer/tools/facialfeatureanalysis/FacialFeature.java
@@ -7,13 +7,23 @@ public enum FacialFeature {
 
 	public static final Color DEFAULT_COLOR = Color.DARK_GRAY;
 
-	private static final Color CHEEKS_COLOR = Color.decode("#4363d8");
-	private static final Color CHIN_COLOR = Color.decode("#42d4f4");
-	private static final Color EYEBROWS_COLOR = Color.decode("#f58231");
-	private static final Color EYESHAPE_COLOR = Color.decode("#911eb4");
-	private static final Color FACESHAPE_COLOR = Color.decode("#3cb44b");
-	private static final Color LIPS_COLOR = Color.decode("#f032e6");
-	private static final Color NOSE_COLOR = Color.decode("#ffe119");
+	private static final Color CHEEKS_COLOR =  Color.decode("#DC143C");
+	private static final Color CHIN_COLOR = Color.decode("#FF7420");
+	private static final Color EYEBROWS_COLOR = Color.decode("#FFCA36");
+	private static final Color EYESHAPE_COLOR = Color.decode("#DCFF65");
+	private static final Color FACESHAPE_COLOR = Color.decode("#14DCB4");
+	private static final Color LIPS_COLOR = Color.decode("#366BFF");
+	private static final Color NOSE_COLOR = Color.decode("#8865FF");
+
+	public static Color getColor(final FacialFeature value, final float similarity) {
+		final var color = getColor(value);
+
+		final int r = Math.min(255, (int) (DEFAULT_COLOR.getRed() + similarity * color.getRed()));
+		final int g = Math.min(255, (int) (DEFAULT_COLOR.getGreen() + similarity * color.getGreen()));
+		final int b = Math.min(255, (int) (DEFAULT_COLOR.getBlue() + similarity * color.getBlue()));
+
+		return new Color(r, g, b, 255);
+	}
 
 	public static Color getColor(final FacialFeature value) {
 		switch (value) {
@@ -31,18 +41,8 @@ public enum FacialFeature {
 				return LIPS_COLOR;
 			case NOSE:
 				return NOSE_COLOR;
+			default:
+				return DEFAULT_COLOR;
 		}
-
-		return Color.WHITE;
-	}
-
-	public static Color getColor(final FacialFeature value, final float similarity) {
-		final var color = getColor(value);
-
-		final int r = Math.min(255, (int) (DEFAULT_COLOR.getRed() + similarity * color.getRed()));
-		final int g = Math.min(255, (int) (DEFAULT_COLOR.getGreen() + similarity * color.getGreen()));
-		final int b = Math.min(255, (int) (DEFAULT_COLOR.getBlue() + similarity * color.getBlue()));
-
-		return new Color(r, g, b, 255);
 	}
 }
