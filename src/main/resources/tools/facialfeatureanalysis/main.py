0c9cba7a03a2b273f0054e83987f5994e1e4d60a
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-17 21:03:27 +0200

    Use sys.exit

--- src/main/resources/tools/facialfeatureanalysis/main.py
+++ src/main/resources/tools/facialfeatureanalysis/main.py
@@ -6,21 +6,25 @@ import sys
 
 if len(sys.argv) < 4:
     print(json.dumps({"error": True, "message": "Invalid arguments"}))
+    sys.exit(1)
 else:
     proband = Individual.parse(sys.argv[1], int(sys.argv[2]))
     if len(sys.argv) > 4:
         try:
             proband = Individual.LIST[sys.argv[4]]
         except:
-            sys.exit("Proband {} not found".format(sys.argv[4]))
+            print(json.dumps({"error": True, "message": "Proband {} not found".format(sys.argv[4])}))
+            sys.exit(1)
 
     result = getFaceAnalysisResult(proband, int(sys.argv[3]))
 
     if "error" in result:
         print(json.dumps(result))
+        sys.exit(1)
     else:
         filepath = sys.argv[1].replace(".json", "-result.json")
         with open(filepath, "w", encoding="utf-8") as file:
             json.dump(result, file, ensure_ascii=False, indent=4)
 
         print(json.dumps({"success": True, "filepath": filepath}))
+        sys.exit(0)
