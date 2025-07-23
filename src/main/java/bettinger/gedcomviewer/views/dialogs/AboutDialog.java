e5d7fbadaa5454cd80562fad971237affdaa9bc2
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-23 11:59:18 +0200

    Added license name to AboutDialog

--- src/main/java/bettinger/gedcomviewer/views/dialogs/AboutDialog.java
+++ src/main/java/bettinger/gedcomviewer/views/dialogs/AboutDialog.java
@@ -43,6 +43,8 @@ public class AboutDialog extends JDialog {
 		HTMLUtils.appendLineBreak(sb);
 		HTMLUtils.appendLine(sb, HTMLUtils.createStrong(String.format("%s \u00A9 2022 %s & %s", I18N.get("Copyright"), HTMLUtils.createEMailLink("c.bettinger@live.de", "Christian Bettinger"), HTMLUtils.createEMailLink("miriam.bettinger@icloud.com", "Miriam Bettinger"))));
 		HTMLUtils.appendLineBreak(sb);
+		HTMLUtils.appendLine(sb, "ISC License");
+		HTMLUtils.appendLineBreak(sb);
 		HTMLUtils.appendLine(sb, "Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.");
 		HTMLUtils.appendLineBreak(sb);
 		HTMLUtils.appendLine(sb, "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.");
