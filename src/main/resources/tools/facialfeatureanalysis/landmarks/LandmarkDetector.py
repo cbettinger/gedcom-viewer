1f18a0203922046f14a575e645fd1b55ea7803e1
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-07-28 12:11:18 +0200

    Formatting

--- src/main/resources/tools/facialfeatureanalysis/landmarks/LandmarkDetector.py
+++ src/main/resources/tools/facialfeatureanalysis/landmarks/LandmarkDetector.py
@@ -5,9 +5,7 @@ from mediapipe.tasks.python import vision
 class LandmarkDetector:
     DETECTOR = vision.FaceLandmarker.create_from_options(
         vision.FaceLandmarkerOptions(
-            base_options=python.BaseOptions(
-                model_asset_path="landmarks/face.task"
-            ),
+            base_options=python.BaseOptions(model_asset_path="landmarks/face.task"),
             output_face_blendshapes=True,
             output_facial_transformation_matrixes=True,
             num_faces=1,
