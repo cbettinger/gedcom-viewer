cc1688f6b822914fbf049f288e0252797549b5ca
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-08-26 21:57:25 +0200

    use rodrigues rotation

--- src/main/python/utils/mathUtils.py
+++ src/main/python/utils/mathUtils.py
@@ -3,8 +3,9 @@ import numpy as np
 def getRotationMatrixToAlignVectors(src, dst):
     a, b = (src / np.linalg.norm(src)).reshape(3), (dst / np.linalg.norm(dst)).reshape(3)
     v = np.cross(a, b)
-    c = np.dot(a, b)
     s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotationMatrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    k = v / s
+    c = np.dot(a, b)
+    kmat = np.array([[0, -k[2], k[1]], [k[2], 0, -k[0]], [-k[1], k[0], 0]])
+    rotationMatrix = np.eye(3) + s*kmat + (1-c) * kmat.dot(kmat)
     return rotationMatrix
