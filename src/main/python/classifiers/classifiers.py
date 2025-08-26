f2b6247bbf017103ca159aa0c3a53c1b6e9d2082
Miriam Sobisiak <sobisiam@hochschule-trier.de>
2025-08-26 21:58:26 +0200

    use new xgb models

--- src/main/python/classifiers/classifiers.py
+++ src/main/python/classifiers/classifiers.py
@@ -1,15 +1,15 @@
 from classifiers.XGBooster import XGBooster
 
 XGB_CLASSIFIERS = {
-    "CHEEKS": {"first": XGBooster("xgb_20_6_cheeks_first", "src/main/resources/xgbModels/xgbs_20_6_cheeks_edgeDifference_first.json", 20, 6),
-               "second": XGBooster("xgb_20_6_cheeks_second", "src/main/resources/xgbModels/xgbs_20_6_cheeks_edgeDifference_second.json", 20, 6)},
-    "CHIN": XGBooster("xgb_20_6_chin", "src/main/resources/xgbModels/xgbs_20_6_chin_edgeDifference.json", 20, 6),
-    "EYEBROWS": {"first": XGBooster("xgb_20_6_eyebrows_first", "src/main/resources/xgbModels/xgbs_20_6_eyebrows_edgeDifference_first.json", 20, 6),
-               "second": XGBooster("xgb_20_6_eyebrows_second", "src/main/resources/xgbModels/xgbs_20_6_eyebrows_edgeDifference_second.json", 20, 6)},
-    "EYESHAPE": {"first": XGBooster("xgb_20_6_eyes_first", "src/main/resources/xgbModels/xgbs_20_6_eyes_edgeDifference_first.json", 20, 6),
-               "second": XGBooster("xgb_20_6_eyes_second", "src/main/resources/xgbModels/xgbs_20_6_eyes_edgeDifference_second.json", 20, 6)},
-    "FACESHAPE": XGBooster("xgb_20_6_faceshape", "src/main/resources/xgbModels/xgbs_20_6_faceshape_edgeDifference.json", 20, 6),
-    "LIPS": {"first": XGBooster("xgb_20_6_lips_first", "src/main/resources/xgbModels/xgbs_20_6_lips_edgeDifference_first.json", 20, 6),
-               "second": XGBooster("xgb_20_6_lips_second", "src/main/resources/xgbModels/xgbs_20_6_lips_edgeDifference_second.json", 20, 6)},
-    "NOSE": XGBooster("xgb_20_6_nose", "src/main/resources/xgbModels/xgbs_20_6_nose_edgeDifference.json", 20, 6)
+    "CHEEKS": {"first": XGBooster("xgb_100_6_cheeks_geometry_first", "src/main/resources/xgbModels/xgb_100_6_cheeks_edgeDifference_geometry_first.json", 20, 6),
+               "second": XGBooster("xgb_100_6_cheeks_geometry_second", "src/main/resources/xgbModels/xgb_100_6_cheeks_edgeDifference_geometry_second.json", 20, 6)},
+    "CHIN": XGBooster("xgb_100_6_chin_geometry", "src/main/resources/xgbModels/xgb_100_6_chin_edgeDifference_geometry.json", 20, 6),
+    "EYEBROWS": {"first": XGBooster("xgb_100_6_eyebrows_geometry_first", "src/main/resources/xgbModels/xgb_100_6_eyebrows_edgeDifference_geometry_first.json", 20, 6),
+               "second": XGBooster("xgb_100_6_eyebrows_geometry_second", "src/main/resources/xgbModels/xgb_100_6_eyebrows_edgeDifference_geometry_second.json", 20, 6)},
+    "EYESHAPE": {"first": XGBooster("xgb_100_6_eyes_geometry_first", "src/main/resources/xgbModels/xgb_100_6_eyes_edgeDifference_geometry_first.json", 20, 6),
+               "second": XGBooster("xgb_100_6_eyes_geometry_second", "src/main/resources/xgbModels/xgb_100_6_eyes_edgeDifference_geometry_second.json", 20, 6)},
+    "FACESHAPE": XGBooster("xgb_100_6_faceshape_geometry", "src/main/resources/xgbModels/xgb_100_6_faceshape_edgeDifference_geometry.json", 20, 6),
+    "LIPS": {"first": XGBooster("xgb_100_6_lips_geometry_first", "src/main/resources/xgbModels/xgb_100_6_lips_edgeDifference_geometry_first.json", 20, 6),
+               "second": XGBooster("xgb_100_6_lips_geometry_second", "src/main/resources/xgbModels/xgb_100_6_lips_edgeDifference_geometry_second.json", 20, 6)},
+    "NOSE": XGBooster("xgb_100_6_nose_geometry", "src/main/resources/xgbModels/xgb_100_6_nose_edgeDifference_geometry.json", 20, 6)
 }
