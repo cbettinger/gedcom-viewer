81ab3b17f7ccc1bc8ad2ec6b0cdab056918503a0
Christian Bettinger <c.bettinger@rock-bettinger.com>
2026-02-12 10:13:53 +0100

    Hide frame title on macOS

--- src/main/java/bettinger/gedcomviewer/views/Frame.java
+++ src/main/java/bettinger/gedcomviewer/views/Frame.java
@@ -7,10 +7,15 @@ import com.formdev.flatlaf.util.SystemInfo;
 public class Frame extends JFrame {
 	@Override
 	public void setVisible(final boolean b) {
-		if (SystemInfo.isMacOS && SystemInfo.isMacFullWindowContentSupported) {
+		if (SystemInfo.isMacOS) {
 			final var rootPane = getRootPane();
-			rootPane.putClientProperty("apple.awt.fullWindowContent", true);
-			rootPane.putClientProperty("apple.awt.transparentTitleBar", true);
+
+			rootPane.putClientProperty("apple.awt.windowTitleVisible", false);
+
+			if (SystemInfo.isMacFullWindowContentSupported) {
+				rootPane.putClientProperty("apple.awt.fullWindowContent", true);
+				rootPane.putClientProperty("apple.awt.transparentTitleBar", true);
+			}
 		}
 
 		super.setVisible(b);
